5eb9c785b3a6f93e55a4ca3334f06f5af6afd6fa
Rishi Raam <rishiraamns@gmail.com>
2024-02-04 10:42:35 +0530

    Create Navbar

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,5 +1,35 @@
-const NavItems = () => {
-    return(<></>)
-}
+"use client";
 
-export default NavItems
+import { PRODUCT_CATEGORIES } from "@/config";
+import { useState } from "react";
+import NavItem from "./NavItem";
+
+const NavItems = () => {
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+  const isAnyOpen = activeIndex !== null;
+  return (
+    <div className="flex gap-4 h-full">
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(i);
+          }
+        };
+        const isOpen = i === activeIndex;
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default NavItems;
